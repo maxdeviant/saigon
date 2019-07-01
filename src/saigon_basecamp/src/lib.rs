45b9ae77606ba1f5f50c6dfb2828d609bbc6aa66
Marshall Bowers <elliott.codes@gmail.com>
2019-07-01 19:41:02 -0400

    Scaffold out the Basecamp chatbot types

--- src/saigon_basecamp/src/lib.rs
+++ src/saigon_basecamp/src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub struct Payload {
+    pub message: String,
+    pub creator: Creator,
+    pub callback_url: String,
+}
+
+pub struct Creator {
+    pub id: i32,
+    pub attachable_sgid: String,
+    pub name: String,
+    pub email_address: String,
+    pub personable_type: String,
+    pub title: String,
+    pub bio: String,
+    pub created_at: String,
+    pub updated_at: String,
+    pub admin: bool,
+    pub owner: bool,
+    pub time_zone: String,
+    pub avatar_url: String,
+    pub company: Company,
+}
+
+pub struct Company {
+    pub id: i32,
+    pub name: String,
 }
