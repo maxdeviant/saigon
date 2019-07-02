fec36d668cba7dca1bf5dbdacc580bc11f1afd24
Marshall Bowers <elliott.codes@gmail.com>
2019-07-01 21:23:22 -0400

    Add `Plugin` trait

--- src/saigon/src/lib.rs
+++ src/saigon/src/lib.rs
@@ -4,12 +4,31 @@ use hyper::rt::Future;
 use hyper::service::service_fn_ok;
 use hyper::{Body, Method, Response, Server, StatusCode};
 
+pub trait Plugin {
+    fn version(&self) -> String;
+}
+
+#[derive(Debug)]
+pub struct Command {
+    pub value: String,
+}
+
+#[derive(Debug)]
+pub struct Message {
+    pub value: String,
+}
+
+pub trait Receive {
+    fn receive(&mut self, command: Command);
+}
+
 pub struct Config {
     addr: SocketAddr,
 }
 
 pub struct Bot {
     config: Config,
+    plugins: Vec<Box<dyn Plugin>>,
 }
 
 impl Bot {
@@ -38,19 +57,28 @@ impl Bot {
     }
 }
 
-#[derive(Debug)]
 pub struct BotBuilder {
     addr: SocketAddr,
+    plugins: Vec<Box<dyn Plugin>>,
 }
 
 impl BotBuilder {
     pub fn new<A: Into<SocketAddr>>(addr: A) -> Self {
-        Self { addr: addr.into() }
+        Self {
+            addr: addr.into(),
+            plugins: Vec::new(),
+        }
+    }
+
+    pub fn add_plugin<P: Plugin + 'static>(&mut self, plugin: P) -> &mut Self {
+        self.plugins.push(Box::new(plugin));
+        self
     }
 
     pub fn build(self) -> Result<Bot, &'static str> {
         Ok(Bot {
             config: Config { addr: self.addr },
+            plugins: self.plugins,
         })
     }
 }
