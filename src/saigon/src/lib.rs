a3e588e5b91510e8c524f78d9c427f152fabb760
Marshall Bowers <elliott.codes@gmail.com>
2019-07-02 17:25:53 -0400

    Use Rocket for the server

--- src/saigon/src/lib.rs
+++ src/saigon/src/lib.rs
@@ -1,8 +1,11 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
 use std::net::SocketAddr;
 
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
-use hyper::{Body, Method, Response, Server, StatusCode};
+use rocket::State;
 
 pub trait Plugin {
     fn version(&self) -> String;
@@ -28,38 +31,21 @@ pub struct Config {
 
 pub struct Bot {
     config: Config,
-    plugins: Vec<Box<dyn Plugin + Sync>>,
+    plugins: Vec<Box<dyn Plugin + Send + Sync>>,
 }
 
 impl Bot {
     pub fn start(self) {
-        let service = || {
-            service_fn_ok(|req| match (req.method(), req.uri().path()) {
-                (&Method::POST, "/") => {
-                    let body = req.into_body();
-
-                    println!("Body is {:?}", body);
-
-                    Response::new(Body::from("Hello"))
-                }
-                _ => Response::builder()
-                    .status(StatusCode::NOT_FOUND)
-                    .body(Body::empty())
-                    .unwrap(),
-            })
-        };
-
-        let server = Server::bind(&self.config.addr)
-            .serve(service)
-            .map_err(|err| println!("Error: {}", err));
-
-        hyper::rt::run(server)
+        rocket::ignite()
+            .manage(self)
+            .mount("/", routes![index])
+            .launch();
     }
 }
 
 pub struct BotBuilder {
     addr: SocketAddr,
-    plugins: Vec<Box<dyn Plugin + Sync>>,
+    plugins: Vec<Box<dyn Plugin + Send + Sync>>,
 }
 
 impl BotBuilder {
@@ -70,7 +56,7 @@ impl BotBuilder {
         }
     }
 
-    pub fn add_plugin(mut self, plugin: Box<dyn Plugin + Sync>) -> Self {
+    pub fn add_plugin(mut self, plugin: Box<dyn Plugin + Send + Sync>) -> Self {
         self.plugins.push(plugin);
         self
     }
@@ -81,4 +67,15 @@ impl BotBuilder {
             plugins: self.plugins,
         })
     }
+}
+
+#[post("/", data = "<payload>")]
+fn index(bot: State<Bot>, payload: String) -> String {
+    println!("Payload is {}", payload);
+
+    for plugin in bot.plugins.iter() {
+        println!("Plugin version: {}", plugin.version());
+    }
+
+    "Hello!".into()
 }
