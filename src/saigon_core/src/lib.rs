0e80cd999e95064d9056c52281f87395f1766870
Marshall Bowers <elliott.codes@gmail.com>
2019-07-23 20:41:36 -0400

    Add documentation for saigon_core types

--- src/saigon_core/src/lib.rs
+++ src/saigon_core/src/lib.rs
@@ -1,24 +1,40 @@
+#![deny(missing_docs)]
+
+//! Contains the core types for Saigon.
+
+/// A Saigon source.
 pub trait Source {
+    /// Returns the source's name.
     fn name(&self) -> String;
 
+    /// Returns the source's version.
     fn version(&self) -> String;
 
+    /// Handles a payload for the source to produce a [`Command`].
     fn handle(&mut self, payload: &str) -> Option<Command>;
 }
 
+/// The response from a [`Plugin`].
 pub enum PluginResponse {
+    /// The plugin ignored the [`Command`].
     Ignore,
 }
 
+/// A Saigon plugin.
 pub trait Plugin {
+    /// Returns the plugin's name.
     fn name(&self) -> String;
 
+    /// Returns the plugin's version.
     fn version(&self) -> String;
 
+    /// Receives a [`Command`] for the plugin to process.
     fn receive(&mut self, command: &Command) -> String;
 }
 
+/// A command given to a [`Plugin`].
 #[derive(Debug)]
 pub struct Command {
+    /// The command payload.
     pub value: String,
 }
